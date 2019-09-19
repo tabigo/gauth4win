f3058962aa33aaf889afe64f34f60d0e712f903a
tabigo <info@tabigo.net>
2019-09-20 00:06:29 +0200

    19-7.9.2019

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root  '#index'
+  root '#index'
   root 'application#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
