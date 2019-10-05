d45d09f6f7cc3b3bbbd3f8dc25c4b8be3f8c1de8
tabigo <info@tabigo.net>
2019-10-06 01:14:35 +0200

    6.10.2019

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_30_214858) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string "content"
+    t.string "text"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
